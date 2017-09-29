a55ae7d53ce2c35bb921f2438fc01b597c1efb37
ijikeman <keiji.ue@gmail.com>
2017-09-29 11:32:44 +0900

    serial

--- serial/serial1/serial1.ino
+++ serial/serial1/serial1.ino
@@ -1,16 +1,17 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(2, 3); // RX, TX
+SoftwareSerial mySerial(10,16); // RX, TX
 
 void setup(){ 
-    mySerial.begin(9600);
+    //mySerial.begin(9600);
+    //Serial.begin(9600);
     Serial1.begin(9600);
-//    pinMode(2, INPUT);
 }
 
 void loop(){
-    mySerial.write(45);
-//    Serial1.write(45);
+//    mySerial.write("HelloWorld");
+//    Serial.write("Hey");
+   Serial1.write("Hey");
     digitalWrite(17, HIGH);
     delay(1000);
     digitalWrite(17, LOW);
--- serial/serial2/serial2.ino
+++ serial/serial2/serial2.ino
@@ -1,21 +1,25 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(2, 3); // RX, TX
+SoftwareSerial mySerial(10, 16); // RX, TX
 
-void setup(){ 
+void setup(){
   Serial.begin(9600);
-//  Serial1.begin(9600);
-    mySerial.begin(9600);
-//    pinMode(2, OUTPUT);  
+  Serial1.begin(9600);
+//    mySerial.begin(9600);
+//  while(!Serial1) {
+//    digitalWrite(17, HIGH);
+//  }
 }
 
 void loop(){
-  while(mySerial.available()) {
+//  while(mySerial.available()) {
 //  while(Serial1.available()) {
-    Serial.println((byte)Serial1.read());
+//char val = mySerial.read();
+char val = Serial1.read();
+Serial.println(val);
     digitalWrite(17, HIGH);
 	delay(1000);
   digitalWrite(17, LOW);
   delay(1000);
-  }
+//  }
 }
