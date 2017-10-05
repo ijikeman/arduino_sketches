7d428c1e1c183e7c4170c0d55b0b8b0c20ebc531
ijikeman <keiji.ue@gmail.com>
2017-10-06 01:19:35 +0900

    add layer0, layer1

--- keyboard/keyboard_layer1/keyboard_layer1.ino
+++ keyboard/keyboard_layer1/keyboard_layer1.ino
@@ -1,22 +1,24 @@
 #define LAYER_BASE 0
-#define LAYER_UPPER 1;
-#define LAYER_LOWER 2;
-const byte LAYER_NUM = 3;
+#define LAYER_UPPER 1
+#define LAYER_LOWER 2
 const byte outputNum = 1;
 const byte inputNum = 1;
-#define K_H 0x63
-#define MO(layer) switch(byte layer)
+
+#define FUNC_LT 1<<12
+#define LT(layer,key) (unsigned int)(FUNC_MO | layer << 8 | key)
+#define KC_H 0x63
 
 byte currentLayer = 0;
 byte beforeLayer = 0;
- const int keyMap[LAYER_NUM][outputNum][inputNum] = {
-  {
-	{MO(2)}
+
+const int keyMap[][outputNum][inputNum] = {
+  [LAYER_BASE] = {
+  	{LT(LAYER_UPPER, KC_H)}
   },
-  {
+  [LAYER_UPPER] = {
 	{2}
   },
-  {
+  [LAYER_LOWER] = {
 	{3}
   }
 };
@@ -26,14 +28,13 @@ void setup() {
 }
 
 void loop() {
-//  Serial.print(keyMap[currentLayer][1][1]);
+  Serial.print(keyMap[currentLayer][0][0]);
 }
 
-void layer() {
-  
+void switchlayer(byte clayer) {
 }
 
-void switch(byte switchLayer) {
-  beforeLayer = currentLayer;
-  currentLayer switchLayer;
-}
+//void switch(byte switchLayer) {
+//  beforeLayer = currentLayer;
+//  currentLayer switchLayer;
+//}
