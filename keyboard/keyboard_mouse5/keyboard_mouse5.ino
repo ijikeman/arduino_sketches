2e37d6c1c8c6d6533d3eecbd93458441c2d9c0b4
geoemon2k <keiji.ue@gmail.com>
2017-10-06 12:28:09 +0900

    BSLS and PLUS

--- keyboard/keyboard_mouse5/keyboard_mouse5.ino
+++ keyboard/keyboard_mouse5/keyboard_mouse5.ino
@@ -12,13 +12,13 @@ const byte keyMap[outputNum*2][inputNum*2]  = {
    {KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T},
    {KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,    KC_G},
    {KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B},
-   {KC_ESC,  NONE,    KC_LALT,   KC_LGUI, NONE, KC_SPC},
+   {KC_ESC,  NONE,    KC_LALT,   KC_LGUI, KC_EQL, KC_SPC},
 
    {KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_MINS},
    {KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC},
    {KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT},
    {KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_QUOT},
-   {KC_BSPC, NONE,    KC_LEFT, KC_DOWN,KC_UP,   KC_RGHT}
+   {KC_BSPC, KC_BSLS,    KC_LEFT, KC_DOWN,KC_UP,   KC_RGHT}
 };
 
 const int mouseButton = 7;    // input pin for the mouse pushButton
