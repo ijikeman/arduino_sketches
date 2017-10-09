82e238c17dbf89f52e494d299afa0023c767d8a8
ijikeman <keiji.ue@gmail.com>
2017-10-10 01:06:25 +0900

    add layer

---
+++ keyboard/keyboard_layer3/keyboard_layer3.ino
@@ -0,0 +1,102 @@
+#include "Keydefine.h"
+#include <Keyboard.h>
+#include "Mouse.h"
+
+// LAYERS
+#define BASE 0
+#define UPPER 1
+#define LOWER 2
+
+//FUNCTIONS
+#define FUNC_LT 1<<12
+
+// FUNCTIONS MACRO
+#define LT(layer,key) (unsigned int)(FUNC_LT | layer << 8 | key)
+
+byte currentLayer = BASE;
+byte beforeLayer = BASE;
+
+// Maxipad
+const int inputNum = 6;
+const int outputNum = 5;
+const int inputPins[inputNum] =  {21, 5, 6, 20, 8, 9};
+const int outputPins[outputNum] ={10, 16, 14, 15, 18};
+const int keyMap[][outputNum*2][inputNum*2]  = {
+  [BASE] = {
+   {KC_GRV, KC_1,    KC_2,    KC_3,   KC_4,    KC_5},
+   {KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T},
+   {KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,    KC_G},
+   {KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B},
+   {KC_ESC,  NONE,    KC_LALT, KC_LGUI,LT(UPPER, NONE), KC_SPC},
+
+   {KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_MINS},
+   {KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC},
+   {KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT},
+   {KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_QUOT},
+   {KC_BSPC, LT(LOWER, NONE),  KC_LEFT, KC_DOWN,KC_UP,   KC_RGHT}
+  },
+
+  [UPPER] = {
+   {_______, _______, _______, _______, _______, _______ },
+   {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC },
+   {_______, _______, _______, _______, _______, _______ },
+   {_______, _______, _______, _______, _______, _______ },
+   {_______, _______, _______, _______, _______, _______ },
+
+   {_______, _______, _______, _______, _______, _______ },
+   {KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQL },
+   {_______, _______, _______, _______, _______, _______ },
+   {_______, _______, _______, _______, _______, _______ },
+   {KC_DEL, _______, _______, _______, _______, _______ }
+  },
+
+  [LOWER] = {
+   {_______, _______, _______, _______, _______, _______ },
+   {KC_GRV, KC_1,    KC_2,    KC_3,   KC_4,    KC_5},
+   {_______, _______, _______, _______, _______, _______ },
+   {_______, _______, _______, _______, _______, _______ },
+   {_______, _______, _______, _______, _______, _______ },
+
+   {_______, _______, _______, _______, _______, _______ },
+   {KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_MINS},
+   {_______, _______, _______, _______, _______, _______ },
+   {_______, _______, _______, _______, _______, _______ },
+   {KC_DEL, _______, _______, _______, _______, _______ }
+  }
+};
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int keyData = keyMap[currentLayer][9][1];
+  int func = keyData >> 12;
+  int layer = (keyData & 0b0000111100000000) >> 8;
+  int keycode = keyData & 0b0000000011111111;
+  Serial.println(keyData);
+  Serial.print(func);
+  Serial.print(":");
+  Serial.print(layer);
+  Serial.print(":");
+  Serial.println(keycode);
+
+  switch(func) {
+    case 0:
+      break;
+    case 1: // Layer Tap
+      beforeLayer = currentLayer;
+      currentLayer = layer;
+      break;
+    default:
+      break;
+   }
+  Serial.print(func);
+  Serial.print(":");
+  Serial.print(currentLayer);
+  Serial.print(beforeLayer);
+  Serial.print(":");
+  Serial.println(keycode);
+  delay(1000);
+}
