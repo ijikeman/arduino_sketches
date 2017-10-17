c6f96b555736d40db347566a9f2d12640dab1faa
ijikeman <keiji.ue@gmail.com>
2017-10-18 00:31:34 +0900

    fixed oop9

--- Oop/oop9/SerialController.cpp
+++ Oop/oop9/SerialController.cpp
@@ -4,12 +4,12 @@
 #define BIT(num) ((unsigned int)1 << (num))
 #define BIT_MK BIT(7)
 #define BIT_PUSH_RELEASE BIT(6)
-#define BIT_ROW BIT(3)
-#define BIT_COL BIT(0)
+#define BIT_ROW (BIT(5)|BIT(4)|BIT(3))
+#define BIT_COL (BIT(2)|BIT(1)|BIT(0))
 
 #define BIT_X_Y BIT(6)
 #define BIT_MINUS_PLUS BIT(5)
-#define BIT_RANGE BIT(0)
+#define BIT_RANGE (BIT(4)|BIT(3)|BIT(2)|BIT(1)|BIT(0))
 
 SerialController::SerialController(unsigned int _beginRate) {
   Serial1.begin(_beginRate);
@@ -18,12 +18,11 @@ SerialController::SerialController(unsigned int _beginRate) {
 void SerialController::makeData(bool _mode, bool _second, unsigned int _third, unsigned int _forth) {
   // Keyboard Mode
   if ( _mode == 0 ) {
-//    data = (_mode & BIT_MK) | (_second & BIT_PUSH_RELEASE) | (_third & BIT_ROW) | (_forth & BIT_COL);
-    data = ((unsigned char)_mode & BIT_MK) | ((unsigned char)_second << 6) | _third << 3 | _forth;
+    data = _mode << 7 | _second << 6 | _third << 3 | _forth;
 
   // Mouse Mode
   } else {
-    data = (_mode & BIT_MK) | (_second & BIT_X_Y) | (_third & BIT_MINUS_PLUS) | (_forth & BIT_RANGE);
+    data = _mode << 7 | _second << 6 | _third & << 5 | _forth;
   }
   Serial.println(data);
 }
